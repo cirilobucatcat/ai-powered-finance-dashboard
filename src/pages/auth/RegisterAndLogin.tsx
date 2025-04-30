941cc39395bed33007671c9837ab3d4cc0acd9f6
Cirilo Bucatcat Jr. <bucatcat1999@gmail.com>
2025-04-30 08:01:05 +0800

    implemented SEO

--- src/pages/auth/RegisterAndLogin.tsx
+++ src/pages/auth/RegisterAndLogin.tsx
@@ -5,18 +5,25 @@ import LoginForm from "./Auth/LoginForm";
 import RegisterForm from "./Auth/RegisterForm";
 import { useAuth } from "@/context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import SEO from "@/components/Seo";
 
 export default function RegisterAndLogin() {
   const [tab, setTab] = useState<'login' | 'register'>('login');
   const { user } = useAuth();
   const navigate = useNavigate();
-  
+
   useEffect(() => {
-    if(user) navigate('/dashboard')
+    if (user) navigate('/dashboard')
   })
 
   return (
     <div className='w-full h-screen grid grid-cols-2 gap-2 bg-lime-50 p-6'>
+      <SEO
+        title="Finebird - Finance Dashboard"
+        description="Your assistant in finance"
+        name="Finebird"
+        type="website"
+      />
       <AuthHero />
       <div className="col-span-full sm:col-span-1 w-full flex flex-col items-center justify-center bg-white shadow rounded-xl p-6">
         <div className="mb-12 font-open-sans bg-lime-100 py-1 px-2 rounded-lg space-x-4">
--- src/pages/main/Dashboard.tsx
+++ src/pages/main/Dashboard.tsx
@@ -1,9 +1,16 @@
+import SEO from "@/components/Seo";
 import { useAuth } from "@/context/AuthContext";
 
 export default function Dashboard() {
   const { logout, user } = useAuth();
   return (
     <>
+      <SEO 
+          title="Dashboard" 
+          description="Finebird Dashboard"
+          name="Finebird"
+          type="website"
+      />
       <p>Welcome, {user?.displayName}</p>
       <button onClick={logout}>Logout</button>
     </>
